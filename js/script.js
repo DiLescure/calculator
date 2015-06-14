4acff8a5e1f2e599f86a390d1a0e6d4d5edd39f9
Di Lescure <yo@dianitica.com>
2015-06-14 03:22:43 -0600

    Operator affects number side

--- js/script.js
+++ js/script.js
@@ -13,25 +13,29 @@ $(function () {
 	$('button').click(function(ev){
 		ev.preventDefault();
 		var button_val = $(ev.target).html();
+		var side = (calc_state.operator === '')? 'left_val' : 'right_val';
+
 		if(/\d/.test(button_val)){
 			button_val = new Big(button_val);
 			if(calc_state.decimal === false){
-				calc_state.left_val = calc_state.left_val.times(10);
+				calc_state[side] = calc_state[side].times(10);
 				if($('#screen').html().length > 9){
-					calc_state.left_val= calc_state.left_val.div(10);
+					calc_state[side] = calc_state[side].div(10);
 				}else{
-					calc_state.left_val = calc_state.left_val.plus(Number(button_val.toPrecision(10)));
-			}
+					calc_state[side] = calc_state[side].plus(Number(button_val.toPrecision(10)));
+				}
 			}else if($('#screen').html().length < 10){
 				button_val = button_val.div(calc_state.decimal_place);
 				calc_state.decimal_place *= 10;
-				calc_state.left_val = calc_state.left_val.plus(Number(button_val.toPrecision(10)));
+				calc_state[side] = calc_state[side].plus(Number(button_val.toPrecision(10)));
 			}
 		}else if(button_val === 'clear'){
 			calc_state.clear();
 		}else if(button_val === '.'){
 			calc_state.decimal = true;
+		}else{
+			calc_state.operator = button_val;
 		}
-		$('#screen').html(Number(calc_state.left_val.toPrecision(10)));
+		$('#screen').html(Number(calc_state[side].toPrecision(10)));
 	});
 });
