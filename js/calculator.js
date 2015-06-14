a5654cfd7824b825075a5754a7f6bfba34fe246c
Di Lescure <yo@dianitica.com>
2015-06-14 01:22:22 -0600

    Subtraction throws error if 1 parameter set

--- js/calculator.js
+++ js/calculator.js
@@ -14,4 +14,7 @@ function subtraction(a, b) {
 	if(typeof a === 'undefined'){
 		throw new Error('Parameter "a" not set!');
 	}
+	if(typeof b === 'undefined'){
+		throw new Error('Parameter "b" not set!');
+	}
 }
--- spec/subtraction_spec.js
+++ spec/subtraction_spec.js
@@ -8,5 +8,15 @@ describe('Subtraction function', function(){
 		}
 
 		expect(error_message).toBe('Parameter "a" not set!');
+
+		error_message = '';
+		try{
+			subtraction(1);
+		}catch(e){
+			error_message = e.message;
+		}
+
+		expect(error_message).toBe('Parameter "b" not set!');
+
 	});
 });
