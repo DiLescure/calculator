9195022279b3516437f6e6ad030f87f593ce8e41
Di Lescure <yo@dianitica.com>
2015-06-14 02:40:26 -0600

    Calculator can click digits and clear

--- js/script.js
+++ js/script.js
@@ -1,7 +1,29 @@
+var CalcState = function(){
+	this.clear = function(){
+		this.right_val = 0;
+		this.left_val = 0;
+		this.operator = '';
+
+	};
+	this.clear();
+};
 $(function () {
+	var calc_state = new CalcState();
 	$('button').click(function(ev){
 		ev.preventDefault();
 		var button_val = $(ev.target).html();
-		$('#screen').html(button_val);
+		if(/\d/.test(button_val)){
+			
+			calc_state.left_val *= 10;
+			if($('#screen').html().length > 9){
+				calc_state.left_val /= 10;
+			}else{
+				calc_state.left_val += parseInt(button_val);
+			}
+			
+		}else if(button_val === 'clear'){
+			calc_state.clear();
+		}
+		$('#screen').html(calc_state.left_val);
 	});
 });
