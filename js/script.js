d78d1203a6dad1e5e81fcc3859e2e45007c147c4
Di Lescure <yo@dianitica.com>
2015-06-14 02:44:51 -0600

    Can use decimals (sorta)

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ var CalcState = function(){
 		this.right_val = 0;
 		this.left_val = 0;
 		this.operator = '';
-
+		this.decimal = false;
+		this.decimal_place = 10;
 	};
 	this.clear();
 };
@@ -13,16 +14,22 @@ $(function () {
 		ev.preventDefault();
 		var button_val = $(ev.target).html();
 		if(/\d/.test(button_val)){
-			
+			if(calc_state.decimal === false){
 			calc_state.left_val *= 10;
 			if($('#screen').html().length > 9){
 				calc_state.left_val /= 10;
 			}else{
 				calc_state.left_val += parseInt(button_val);
 			}
-			
+			}else if($('#screen').html().length < 10){
+				button_val = parseInt(button_val) / calc_state.decimal_place;
+				calc_state.decimal_place *= 10;
+				calc_state.left_val += button_val;
+			}
 		}else if(button_val === 'clear'){
 			calc_state.clear();
+		}else if(button_val === '.'){
+			calc_state.decimal = true;
 		}
 		$('#screen').html(calc_state.left_val);
 	});
