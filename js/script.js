fb4319b7115912b47a3f07c2c95ceab6da898311
Di Lescure <yo@dianitica.com>
2015-06-14 03:13:40 -0600

    Implemented big.js on script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var CalcState = function(){
 	this.clear = function(){
-		this.right_val = 0;
-		this.left_val = 0;
+		this.right_val = new Big(0);
+		this.left_val = new Big(0);
 		this.operator = '';
 		this.decimal = false;
 		this.decimal_place = 10;
@@ -14,23 +14,24 @@ $(function () {
 		ev.preventDefault();
 		var button_val = $(ev.target).html();
 		if(/\d/.test(button_val)){
+			button_val = new Big(button_val);
 			if(calc_state.decimal === false){
-			calc_state.left_val *= 10;
-			if($('#screen').html().length > 9){
-				calc_state.left_val /= 10;
-			}else{
-				calc_state.left_val += parseInt(button_val);
+				calc_state.left_val = calc_state.left_val.times(10);
+				if($('#screen').html().length > 9){
+					calc_state.left_val= calc_state.left_val.div(10);
+				}else{
+					calc_state.left_val = calc_state.left_val.plus(Number(button_val.toPrecision(2)));
 			}
 			}else if($('#screen').html().length < 10){
-				button_val = parseInt(button_val) / calc_state.decimal_place;
+				button_val = button_val.div(calc_state.decimal_place);
 				calc_state.decimal_place *= 10;
-				calc_state.left_val += button_val;
+				calc_state.left_val = calc_state.left_val.plus(Number(button_val.toPrecision(2)));
 			}
 		}else if(button_val === 'clear'){
 			calc_state.clear();
 		}else if(button_val === '.'){
 			calc_state.decimal = true;
 		}
-		$('#screen').html(calc_state.left_val);
+		$('#screen').html(Number(calc_state.left_val.toPrecision(2)));
 	});
 });
